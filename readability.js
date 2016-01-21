176352deaf2ec735f5eddee51c4d0aabda1906a5
Torgeir Hovden <torgeir@urbanexpression.net>
2016-01-21 15:13:46 +0100

    Don't crash if no syllables in word. Closes signatu/signatu#400

--- readability.js
+++ readability.js
@@ -8,7 +8,8 @@ var countSyllables = function(word) {
 	if(word.length <= 3) { return 1; }
 	word = word.replace(/(?:[^laeiouy]es|ed|[^laeiouy]e)$/, '');
 	word = word.replace(/^y/, '');
-	return word.match(/[aeiouy]{1,2}/g).length;
+	var matches = word.match(/[aeiouy]{1,2}/g);
+	return matches ? matches.length : 0;
 };
 
 // Automated Readability Index
