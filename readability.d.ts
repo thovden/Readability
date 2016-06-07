4d4a293a7a10674ad759cab9d340883708424574
Torgeir Hovden <torgeir@urbanexpression.net>
2016-06-07 14:30:04 +0200

    Add module

--- readability.d.ts
+++ readability.d.ts
@@ -1,6 +1,8 @@
-export function automatedReadability(text: string): number;
-export function fleschKincaidEase(text: string): number;
-export function fleschKincaidGradeLevel(text: string): number;
-export function gunningFog(text: string): number;
-export function smogIndex(text: string): number;
-export function colemanLiau(text: string): number;
+declare module "readability" {
+  export function automatedReadability(text: string): number;
+  export function fleschKincaidEase(text: string): number;
+  export function fleschKincaidGradeLevel(text: string): number;
+  export function gunningFog(text: string): number;
+  export function smogIndex(text: string): number;
+  export function colemanLiau(text: string): number;
+}
