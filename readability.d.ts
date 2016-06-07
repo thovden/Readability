6dedc87a419c3b58468144b0fca5c0099947a963
Torgeir Hovden <torgeir@urbanexpression.net>
2016-06-07 14:24:28 +0200

    Typescript definitions

---
+++ readability.d.ts
@@ -0,0 +1,6 @@
+export function automatedReadability(text: string): number;
+export function fleschKincaidEase(text: string): number;
+export function fleschKincaidGradeLevel(text: string): number;
+export function gunningFog(text: string): number;
+export function smogIndex(text: string): number;
+export function colemanLiau(text: string): number;
